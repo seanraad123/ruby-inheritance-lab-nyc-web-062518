5fde3a0b0dbdf693732aea85c4c3ff0855e1f8cd
Sean Conrad <seanconrad123@gmail.com>
2018-07-02 02:01:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+require_relative "./user.rb"
+require 'pry'
+
+class Student < User
+
+  def initialize(first_name, last_name)
+    @knowledge = []
+  end
 
 end
+sean = Student.new("sean", "conrad")
---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = 'first_name'
+    @last_name = 'last_name'
+  end
+
+  def test
+    puts "HELLO"
+  end
+
+end
